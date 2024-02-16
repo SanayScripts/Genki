9017f6765ed4498347e7b61885b558622c7760d9
Sanay Patil <patilsanay1275@gmail.com>
2024-02-16 23:30:43 +0530

    Both arm support added to Main!

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ mp_pose = mp.solutions.pose #grabbing pose estimation models
 cap = cv2.VideoCapture(0) # Setting our video cap device to device '0'
 
 # Curl Counter variables
-counter = 0
-stage = None
+counter_l = 0
+counter_r = 0
+stage_l = None
+stage_r = None
 
 ## Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
@@ -25,65 +27,90 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
         image.flags.writeable = True  # Make the image writable again for further processing
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # Recolor back to BGR
 
+        # Writing a function to calculate the angle
+        def calculate_angle(a,b,c):
+            a = np.array(a) # Start
+            b = np.array(b) # Mid
+            c = np.array(c) # End
+    
+            radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+            angle = np.abs(radians*180.0/np.pi) # Calculating the angle itself
+                
+            if angle >180.0: # Making the angle based on 180 degrees instead of 360
+                angle = 360-angle
+                
+            return angle
+
         # Extract landmarks
         try:
             landmarks = results.pose_landmarks.landmark
 
-            # Get left curl coords
+            # Get coordinates for both arms
             l_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
             l_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
             l_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
-            # Writing a function to calculate the angle
-            def calculate_angle(a,b,c):
-                a = np.array(a) # Start
-                b = np.array(b) # Mid
-                c = np.array(c) # End
-        
-                radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-                angle = np.abs(radians*180.0/np.pi) # Calculating the angle itself
-                    
-                if angle >180.0: # Making the angle based on 180 degrees instead of 360
-                    angle = 360-angle
-                    
-                return angle
+            r_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            r_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            r_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
 
-           # Using the function to calculate angles
-            angle = calculate_angle(l_shoulder, l_elbow, l_wrist)
+            # Calculate angles for both arms
+            angle_l = calculate_angle(l_shoulder, l_elbow, l_wrist)
+            angle_r = calculate_angle(r_shoulder, r_elbow, r_wrist)
 
-            # Visualize angle
-            cv2.putText(image, str(angle),
+            # Visualize angles
+            cv2.putText(image, str(angle_l),
                         tuple(np.multiply(l_elbow, [640, 480]).astype(int)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
-           
 
-            # Curl counter logic
-            if angle > 160:
-                stage = "down"
-            if angle < 30 and stage == "down":
-                stage = "up"
-                counter += 1
-                
+            cv2.putText(image, str(angle_r),
+                        tuple(np.multiply(r_elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)          
+
+            # Curl counter logic for both arms
+            if angle_l > 160:
+                stage_l = "down"
+            if angle_l < 30 and stage_l == "down":
+                stage_l = "up"
+                counter_l += 1
+
+            if angle_r > 160:
+                stage_r = "down"
+            if angle_r < 30 and stage_r == "down":
+                stage_r = "up"
+                counter_r += 1
 
         except:
             pass
 
         # Render curl counter
-        # Setup status box
-        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1) # Rectangle startpoint, endpoint, colour and linewidth
+        # Create a black rectangle for the status box
+        cv2.rectangle(image, (0, 0), (360, 73), (0, 0, 0), -1)
 
-        #Rep data
-        cv2.putText(image, 'REPS', (15,12),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, str(counter), (10,60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
-        
-        # Stage Data
-        cv2.putText(image, 'STAGE', (80,12),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
-        cv2.putText(image, stage, (75,60),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 2, cv2.LINE_AA)
+        # Rep count for left arm
+        cv2.putText(image, 'REPS L', (15, 24),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter_l), (15, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
+        # Stage info for left arm
+        cv2.putText(image, 'STAGE L', (100, 24),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image, stage_l, (100, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+        # Rep count for right arm
+        cv2.putText(image, 'REPS R', (200, 24),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter_r), (200, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+
+        # Stage info for right arm
+        cv2.putText(image, 'STAGE R', (275, 24),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image, stage_r, (275, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+  
         # Render detections
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
         cv2.imshow('Mediapipe Feed', image) #popup window
